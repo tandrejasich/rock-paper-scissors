e5a5a5ba6134de0ba64b28c3bf79070e4f9663da
tandrejasich <tandrejasich@gmail.com>
2022-04-20 20:44:07 -0500

    refactor format of playRound to consolidate if/then statements

--- game.js
+++ game.js
@@ -2,42 +2,48 @@
 
 /*start with function called Computer Play that will return Rock paper scissors*/
 
+/*set array of possible outcomes*/
 const outcomes = ['rock','paper','scissors'];
 
 
+/*create function to simulate the computer pick by selecting a random item from the array*/
 function computerPlay() {
 /*pick number between 1 and 3*/
     let selection = Math.floor(Math.random() * 3);
+    /*pick from array*/
     let outcome = outcomes[selection];
+    /*return result to console*/
     return outcome;
     
 }
 
+
+/*set function result as variable for further use*/
 let computerSelection = computerPlay().toLowerCase();
+
+/*testing purposes only, set player selection*/
 let playerSelection = outcomes[0].toLowerCase();
 
-
+/*set function to take in too parameters ... player selection and comp selection*/
 function playRound(playerSelection , computerSelection) {
     /*run through outcomes if user picks rock*/
     console.log("You picked..." + playerSelection.toUpperCase());
     console.log("The computer picked..." + computerSelection.toUpperCase());
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log("ROCK BEATS SCISSORS - YOU WIN!");
-    } else if (playerSelection == 'rock' && computerSelection == 'paper'){
-        console.log("Sorry, Boss... paper beats rock... you lose!");
-    /*paper flows*/
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log("PAPER BEATS ROCK - YOU WIN!");
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors'){
-        console.log("Sorry, Boss... scissors beats paper... you lose!");
-    /*scissor flows*/
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log("SCISSORS BEATS PAPER - YOU WIN!");
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock'){
-        console.log("Sorry, Boss... rock beats scissors... you lose!");
+    if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
+    ) {
+        return(`${playerSelection.toUpperCase()} BEATS ${computerSelection.toUpperCase()}!! YOU WIN!`);
+    } else if (
+        (playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'rock')
+    ) {
+        return(`Sorry, Boss... ${computerSelection} beats ${playerSelection}... you lose!`);
     } else {
-        console.log("YOU TIED - try again!")
+        return("YOU TIED - try again!")
     }
 }
 
-playRound(playerSelection,computerSelection);
+console.log(playRound(playerSelection,computerSelection));
